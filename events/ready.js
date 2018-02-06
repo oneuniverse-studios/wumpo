754d2888567d3e45c86bfa56e05228c69a6ce109
zPr0D3v <36106780+zPr0D3v@users.noreply.github.com>
2018-02-05 19:23:57 -0600

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,4 @@
-
+exports.run = (client) => {
+  client.user.setPresence({ game: { name: 'you', type: 2 } });
+  console.log(`Ready to server in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
+}
